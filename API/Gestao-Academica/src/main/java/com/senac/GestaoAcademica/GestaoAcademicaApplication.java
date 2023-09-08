ba46ab637d9e214173e2fd670b8a3bccfaa22f9a
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-09-08 11:19:57 -0300

    Criação do projeto da API

---
+++ API/Gestao-Academica/src/main/java/com/senac/GestaoAcademica/GestaoAcademicaApplication.java
@@ -0,0 +1,13 @@
+package com.senac.GestaoAcademica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAcademicaApplication.class, args);
+	}
+
+}
---
+++ API/Gestao-Academica/src/test/java/com/senac/GestaoAcademica/GestaoAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.GestaoAcademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
